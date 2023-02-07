d6f7029d457168fbeb6333862cff282ad8938688
Jerome Parent <jejmule@gmail.com>
2023-02-07 12:31:48 +0100

    frequency-first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <TimerOne.h>
+#include <TimerThree.h>
 #include <Ewma.h>
 
 //debug mode (1: on, 0: off) -> print data on serial port
@@ -14,10 +14,10 @@ volatile unsigned int counter = 0;
 //data available tag
 volatile boolean data_available = false;
 //degin Ewma filter
-Ewma adcFilter1(0.1);
+Ewma adcFilter1(0.5);
 
 //vicon acquisition rate
-const unsigned int f_vicon = 200; //Hz
+const unsigned int f_vicon = 10; //Hz
 //compute max_speed in pulse per ms
 //motor speed is 1500 tr/min at 50Hz, the VFD drives the motor up to 100Hz -> motor speed is 3000 tr/min
 //there are 26 tooth on motor pulley
@@ -36,15 +36,17 @@ void setup() {
   //init analog pin
   pinMode(analogPin, OUTPUT);
   //init interupt
-  attachInterrupt(digitalPinToInterrupt(digitalPin), on_interupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(digitalPin), on_interupt,CHANGE);
   //init timer
-  Timer1.initialize(1e6/200); //5ms -> 200Hz (vicon acquisition rate) 
+  Timer3.initialize(1e6/f_vicon); //5ms -> 200Hz (vicon acquisition rate) 
+  Timer3.attachInterrupt(on_timer);
   //init dac
   analogWrite(analogPin,0);  
 }
 
 //loop function
 void loop() {
+  //Serial.println("loop"+String(data_available));
   //update frequency if data available
   if(data_available) {
     //compute frequency
@@ -52,16 +54,20 @@ void loop() {
     //filter freqeuncy
     float frequency_Hz_ewma = adcFilter1.filter(frequency_Hz);
     //map analog value
-    int val = map(frequency_Hz_ewma,0,max_frequency_Hz,0,255);
+    //int val = map(frequency_Hz_ewma,0,max_frequency_Hz,0,255);
+    int pulses_20kmh = 126;
+    byte val = map(counter,0,pulses_20kmh,0,255);
     //write analog value
     analogWrite(analogPin,val);
     
     //print data on serial port
     if (debug) {
+      //print counter for debug
+      Serial.println(">counter:"+String(counter));
       //print period_us for teleplot
-      Serial.println(">period_us:"+String(frequency_Hz));
+      Serial.println(">frequency_Hz:"+String(frequency_Hz));
       //print period_us_ewma for debug
-      Serial.println(">period_us_ewma:"+String(frequency_Hz_ewma));
+      Serial.println(">frequency_Hz_ewma:"+String(frequency_Hz_ewma));
       //print val for debug
       Serial.println(">val:"+String(val));
     }
@@ -74,7 +80,7 @@ void loop() {
 
 //ISR function call by interupt
 void on_interupt() {
-  if(!data_available) {
+  if(true) {
     //update counter
     counter++;
   }
